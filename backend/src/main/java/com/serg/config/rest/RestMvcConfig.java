0e7251d7d613a7a74d444f9c70efcc95333e917e
Sergii Illichevskyi <Sergiy.Illichevskyy@gmail.com>
2016-10-30 15:20:24 +0100

    add rest config

---
+++ backend/src/main/java/com/serg/config/rest/RestMvcConfig.java
@@ -0,0 +1,19 @@
+package com.serg.config.rest;
+
+import com.serg.entities.Author;
+import com.serg.entities.Course;
+import com.serg.entities.User;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RestMvcConfig extends RepositoryRestConfigurerAdapter {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Author.class);
+        config.exposeIdsFor(Course.class);
+        config.exposeIdsFor(User.class);
+        config.setBasePath("/api");
+        config.setReturnBodyForPutAndPost(true);
+    }
+}
