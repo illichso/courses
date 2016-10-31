1958d56b83a573ee1d94ba78ad44d239edae230e
Sergii Illichevskyi <Sergiy.Illichevskyy@gmail.com>
2016-10-31 14:01:04 +0100

    add mongo scripts

---
+++ mongoDB/author.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+use courses;
+
+db.createCollection('author');
+
+db.author.insertMany
+([
+  {
+    id: 'cory-house',
+    firstName: 'Cory',
+    lastName: 'House'
+  },
+  {
+    id: 'scott-allen',
+    firstName: 'Scott',
+    lastName: 'Allen'
+  },
+  {
+    id: 'dan-wahlin',
+    firstName: 'Dan',
+    lastName: 'Wahlin'
+  }
+]);
