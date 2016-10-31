e7b0f6360a728b14715dc7cdce50cf231bb2a64a
Sergii Illichevskyi <Sergiy.Illichevskyy@gmail.com>
2016-10-31 14:52:56 +0100

    remove unneeded Repository annotation

--- backend/src/main/java/com/serg/dao/AuthorRepository.java
+++ backend/src/main/java/com/serg/dao/AuthorRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 @RepositoryRestResource
 public interface AuthorRepository extends MongoRepository<Author, String> {
 }
--- backend/src/main/java/com/serg/dao/CourseRepository.java
+++ backend/src/main/java/com/serg/dao/CourseRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
 @RepositoryRestResource
 public interface CourseRepository extends MongoRepository<Course, String> {
 }
