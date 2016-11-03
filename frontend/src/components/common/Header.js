043aad017c22638eab6f8cac9aebc695b5f8664b
Sergii Illichevskyi <Sergiy.Illichevskyy@gmail.com>
2016-11-03 15:08:01 +0100

    return loading dots to header with warning

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -12,6 +12,7 @@ const Header = ({loading, coursesCount, authorsCount}) => {
       <Link to="/courses" activeClassName="active">Courses ({coursesCount})</Link>
       {" | "}
       <Link to="/about" activeClassName="active">About</Link>
+      {loading && <LoadingDots interval={100} dots={20}/>}
     </nav>
   );
 };
