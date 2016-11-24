41e4f473da32f16369b7604d8b8a07634c875e44
illichso <illichso@gmail.com>
2016-11-24 16:00:23 +0100

    update Auth resourse

--- backend/src/main/java/com/serg/config/auth/AuthenticationResource.java
+++ backend/src/main/java/com/serg/config/auth/AuthenticationResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
+import static java.lang.String.valueOf;
 import static org.springframework.security.core.context.SecurityContextHolder.getContext;
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -20,30 +21,37 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RestController()
 @RequestMapping("/api/session")
 public class AuthenticationResource {
-    private final AuthenticationManager authenticationManager;
+	private final AuthenticationManager authenticationManager;
 
-    @Autowired
-    public AuthenticationResource(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-    }
+	@Autowired
+	public AuthenticationResource(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
 
-    @RequestMapping(method = POST)
-    public LoginUser login(@RequestBody Credentials credentials, HttpSession httpSession) {
-        Authentication authentication = new UsernamePasswordAuthenticationToken(credentials.getUsername(), credentials.getPassword());
-        getContext().setAuthentication(authenticationManager.authenticate(authentication));
+	@RequestMapping(method = POST)
+	public LoginUser login(@RequestBody Credentials credentials, HttpSession httpSession) {
+		Authentication authentication = new UsernamePasswordAuthenticationToken(credentials.getUsername(), credentials.getPassword());
+		getContext().setAuthentication(authenticationManager.authenticate(authentication));
 
-        LoginUser loginUser = new LoginUser(credentials.getUsername(), httpSession.getId(), true);
-        httpSession.setAttribute("loginUser", loginUser);
-        return loginUser;
-    }
+		LoginUser loginUser = new LoginUser(credentials.getUsername(), httpSession.getId(), true);
+		httpSession.setAttribute(getAttributeSessionName(), loginUser);
+		return loginUser;
+	}
 
-    @RequestMapping(method = GET)
-    public Object session(HttpSession session) {
-        return session.getAttribute("user");
-    }
+	@RequestMapping(method = GET)
+	public Object session(HttpSession session) {
+		String className = valueOf(LoginUser.class.getSimpleName());
+		className = className.substring(0, 1).toLowerCase() + className.substring(1);
+		return session.getAttribute(getAttributeSessionName());
+	}
 
-    @RequestMapping(method = DELETE)
-    public void logout(HttpSession session) {
-        session.invalidate();
-    }
+	@RequestMapping(method = DELETE)
+	public void logout(HttpSession session) {
+		session.invalidate();
+	}
+
+	private String getAttributeSessionName() {
+		String className = valueOf(LoginUser.class.getSimpleName());
+		return className.substring(0, 1).toLowerCase() + className.substring(1);
+	}
 }
