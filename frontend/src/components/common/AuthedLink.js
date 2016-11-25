eb3b3e930bf13b9636e4e4747390a5ff723f5f38
illichso <illichso@gmail.com>
2016-11-25 21:33:57 +0100

    refactor

--- frontend/src/components/common/AuthedLink.js
+++ frontend/src/components/common/AuthedLink.js
@@ -1,3 +1,6 @@
+import React from "react";
+import {Link} from "react-router";
+
 import {VisibleToUser} from "../../accessors/accessors";
 
 export const AuthedLink = VisibleToUser(({onClick, to ="#", label, activeClassName = "active"}) => {
