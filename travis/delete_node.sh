ecf4672122d6e56e65c0eb91df143ac2da8fe6c7
illichso <illichso@gmail.com>
2016-12-03 23:29:34 +0100

    revert supress node delete output

--- travis/delete_node.sh
+++ travis/delete_node.sh
@@ -4,35 +4,35 @@ echo "Finding node before deleting";
 node -v;
 npm -v;
 echo "Finding and deleting node, node_modules, npm";
-sudo find / -name "nvm" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name "nvm-*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name "nvm*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name "nvm.*" -exec rm -r "{}" \ &>/dev/null;
+sudo find / -name "nvm" -exec rm -r "{}" \;
+sudo find / -name "nvm-*" -exec rm -r "{}" \;
+sudo find / -name "nvm*" -exec rm -r "{}" \;
+sudo find / -name "nvm.*" -exec rm -r "{}" \;
 
-sudo find / -name ".nvm" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name ".nvm-*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name ".nvm*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name ".nvm.*" -exec rm -r "{}" \ &>/dev/null;
+sudo find / -name ".nvm" -exec rm -r "{}" \;
+sudo find / -name ".nvm-*" -exec rm -r "{}" \;
+sudo find / -name ".nvm*" -exec rm -r "{}" \;
+sudo find / -name ".nvm.*" -exec rm -r "{}" \;
 
-sudo find / -name "node" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name "node-*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name "node_*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name "node.*" -exec rm -r "{}" \ &>/dev/null;
+sudo find / -name "node" -exec rm -r "{}" \;
+sudo find / -name "node-*" -exec rm -r "{}" \;
+sudo find / -name "node_*" -exec rm -r "{}" \;
+sudo find / -name "node.*" -exec rm -r "{}" \;
 
-sudo find / -name ".node" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name ".node-*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name ".node_*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name ".node.*" -exec rm -r "{}" \ &>/dev/null;
+sudo find / -name ".node" -exec rm -r "{}" \;
+sudo find / -name ".node-*" -exec rm -r "{}" \;
+sudo find / -name ".node_*" -exec rm -r "{}" \;
+sudo find / -name ".node.*" -exec rm -r "{}" \;
 
-sudo find / -name "npm" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name "npm-*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name "npm*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name "npm.*" -exec rm -r "{}" \ &>/dev/null;
+sudo find / -name "npm" -exec rm -r "{}" \;
+sudo find / -name "npm-*" -exec rm -r "{}" \;
+sudo find / -name "npm*" -exec rm -r "{}" \;
+sudo find / -name "npm.*" -exec rm -r "{}" \;
 
-sudo find / -name ".npm" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name ".npm-*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name ".npm*" -exec rm -r "{}" \ &>/dev/null;
-sudo find / -name ".npm.*" -exec rm -r "{}" \ &>/dev/null;
+sudo find / -name ".npm" -exec rm -r "{}" \;
+sudo find / -name ".npm-*" -exec rm -r "{}" \;
+sudo find / -name ".npm*" -exec rm -r "{}" \;
+sudo find / -name ".npm.*" -exec rm -r "{}" \;
 
 #sudo find / -name "nvm" -o -name ".nvm" -o -name "node" -o -name ".node" -o -name "node_modules" -o -name ".npm"  -o -name "npm*" -exec rm -r "{}" \;
 
