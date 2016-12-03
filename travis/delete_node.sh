564251781bda55f9252588516bbccaaa14e678ac
illichso <illichso@gmail.com>
2016-12-04 00:02:51 +0100

    remove unneedded node delete configd

--- travis/delete_node.sh
+++ travis/delete_node.sh
@@ -5,9 +5,6 @@ node -v;
 npm -v;
 echo "Finding and deleting node, node_modules, npm";
 sudo find / -name "nvm" -exec rm -r "{}" \;
-
-
-sudo find / -name "nvm" -exec rm -r "{}" \;
 sudo find / -name "nvm-*" -exec rm -r "{}" \;
 sudo find / -name "nvm_*" -exec rm -r "{}" \;
 sudo find / -name "nvm.*" -exec rm -r "{}" \;
