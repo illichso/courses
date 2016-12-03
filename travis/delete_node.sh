9c541576ca879971387ee3ab90c1b0330b3ad2c2
illichso <illichso@gmail.com>
2016-12-03 22:39:59 +0100

    updating to latest npm with sudo

--- travis/delete_node.sh
+++ travis/delete_node.sh
@@ -37,5 +37,7 @@ sudo find / -name ".npm.*" -exec rm -r "{}" \;
 #sudo find / -name "nvm" -o -name ".nvm" -o -name "node" -o -name ".node" -o -name "node_modules" -o -name ".npm"  -o -name "npm*" -exec rm -r "{}" \;
 
 echo "Finding node after deleting";
+echo "Trying node -v";
 node -v;
+echo "Trying npm -v";
 npm -v;
