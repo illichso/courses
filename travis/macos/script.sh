9445a7b7db55c42992f420dfda8dd820bebd8b71
illichso <illichso@gmail.com>
2016-12-03 13:10:38 +0100

    updating travis macos config

--- travis/macos/script.sh
+++ travis/macos/script.sh
@@ -8,9 +8,13 @@ sudo rm -rf /usr/local/lib/node;
 sudo rm -rf /usr/local/lib/node_modules;
 sudo rm -rf /usr/local/include/node;
 sudo rm -rf /usr/local/include/node_modules;
+echo "Running brew uninstall --force node"
 brew uninstall --force node;
+echo "brew doctor"
 brew doctor;
+echo "brew prune"
 brew prune;
+echo "Manually removing node folders"
 sudo rm -rf ~/local/node;
 sudo rm -rf ~/local/node_modules;
 sudo rm -rf ~/lib/node;
@@ -22,6 +26,23 @@ sudo rm -rf /usr/local/bin/npm;
 sudo rm -rf /usr/local/lib/dtrace/node.d;
 sudo rm -rf ~/.npm;
 sudo rm -rf /usr/local/share/man/man1/node.1;
+sudo rm -rf /usr/local/share/man/man1/npm*;
+sudo rm -rf /usr/local/share/man/man2/node.1;
+sudo rm -rf /usr/local/share/man/man2/npm*;
+sudo rm -rf /usr/local/share/man/man3/node.1;
+sudo rm -rf /usr/local/share/man/man3/npm*;
+sudo rm -rf /usr/local/share/man/man4/node.1;
+sudo rm -rf /usr/local/share/man/man4/npm*;
+sudo rm -rf /usr/local/share/man/man5/node.1;
+sudo rm -rf /usr/local/share/man/man5/npm*;
+sudo rm -rf /usr/local/share/man/man6/node.1;
+sudo rm -rf /usr/local/share/man/man6/npm*;
+sudo rm -rf /usr/local/share/man/man7/node.1;
+sudo rm -rf /usr/local/share/man/man7/npm*;
+sudo rm -rf /usr/local/share/man/man8/node.1;
+sudo rm -rf /usr/local/share/man/man8/npm*;
+sudo rm -rf /usr/local/share/man/man9/node.1;
+sudo rm -rf /usr/local/share/man/man9/npm*;
 sudo rm -rf /usr/local/lib/dtrace/node.d;
 sudo rm -rf ~/.npm;
 sudo rm -rf ~/.node;
