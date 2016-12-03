3369e39bd4340ad4b53f20c1b2f55bf0a82dbb52
illichso <illichso@gmail.com>
2016-12-03 21:26:44 +0100

    add pwd

--- travis/macos/script.sh
+++ travis/macos/script.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
 
-chmod +x ./predelete_node.sh;
-bash ./predelete_node.sh;
+pwd;
+ls -la -a;
+chmod +x ./travis/macos/predelete_node.sh;
+bash ./travis/macos/predelete_node.sh;
 
-bash ../delete_node.sh;
+pwd;
+ls -la -a;
+bash ./travis/delete_node.sh;
 
-chmod +x ./install_node.sh;
-bash ./install_node.sh;
+pwd;
+ls -la -a;
+chmod +x ./travis/macos/install_node.sh;
+bash ./travis/macos/install_node.sh;
