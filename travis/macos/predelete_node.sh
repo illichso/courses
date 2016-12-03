e0f050161048ecc86ab4a1ac43c1c8a685456f0c
illichso <illichso@gmail.com>
2016-12-03 22:29:03 +0100

    install latest npm wo s && add brew node uninstall

--- travis/macos/predelete_node.sh
+++ travis/macos/predelete_node.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env sh
 
 echo "Running brew uninstall --force node"
+brew uninstall --force nvm;
+brew uninstall --force n;
+brew uninstall --force nodebrew;
+brew uninstall --force node-build;
+brew uninstall --force nodenv;
+brew uninstall --force nodeenv;
+brew uninstall --force nave;
+brew uninstall --force llnode;
 brew uninstall --force node;
+echo "brew prune"
+brew prune;
 echo "brew doctor"
 brew doctor;
 echo "brew prune"
