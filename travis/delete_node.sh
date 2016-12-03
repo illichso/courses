60c740aad0a81248d456c69a6e3a1d748539da35
illichso <illichso@gmail.com>
2016-12-03 21:53:14 +0100

    use delete node inline

--- travis/delete_node.sh
+++ travis/delete_node.sh
@@ -4,9 +4,13 @@ echo "Finding node before deleting";
 node -v;
 npm -v;
 echo "Finding and deleting node, node_modules, npm";
-sudo find / -name "node" -exec rm -r "{}" \;
-sudo find / -name "node_modules" -exec rm -r "{}" \;
-sudo find / -name "npm*" -exec rm -r "{}" \;
+#sudo find / -name "nvm" -exec rm -r "{}" \;
+#sudo find / -name "node" -exec rm -r "{}" \;
+#sudo find / -name "node_modules" -exec rm -r "{}" \;
+#sudo find / -name "npm*" -exec rm -r "{}" \;
+
+sudo find / -name "nvm" -o -name "node" -o -name "node_modules" -o -name "npm*" -exec rm -r "{}" \;
+
 echo "Finding node after deleting";
 node -v;
 npm -v;
