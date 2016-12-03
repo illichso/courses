b452333f719d86418ed72e0acc839fadbd287850
illichso <illichso@gmail.com>
2016-12-03 22:03:14 +0100

    extend delete patterns

--- travis/delete_node.sh
+++ travis/delete_node.sh
@@ -4,12 +4,37 @@ echo "Finding node before deleting";
 node -v;
 npm -v;
 echo "Finding and deleting node, node_modules, npm";
-#sudo find / -name "nvm" -exec rm -r "{}" \;
-#sudo find / -name "node" -exec rm -r "{}" \;
-#sudo find / -name "node_modules" -exec rm -r "{}" \;
-#sudo find / -name "npm*" -exec rm -r "{}" \;
+sudo find / -name "nvm" -exec rm -r "{}" \;
+sudo find / -name "nvm-*" -exec rm -r "{}" \;
+sudo find / -name "nvm*" -exec rm -r "{}" \;
+sudo find / -name "nvm.*" -exec rm -r "{}" \;
 
-sudo find / -name "nvm" -o -name "node" -o -name "node_modules" -o -name "npm*" -exec rm -r "{}" \;
+sudo find / -name ".nvm" -exec rm -r "{}" \;
+sudo find / -name ".nvm-*" -exec rm -r "{}" \;
+sudo find / -name ".nvm*" -exec rm -r "{}" \;
+sudo find / -name ".nvm.*" -exec rm -r "{}" \;
+
+sudo find / -name "node" -exec rm -r "{}" \;
+sudo find / -name "node-*" -exec rm -r "{}" \;
+sudo find / -name "node_*" -exec rm -r "{}" \;
+sudo find / -name "node.*" -exec rm -r "{}" \;
+
+sudo find / -name ".node" -exec rm -r "{}" \;
+sudo find / -name ".node-*" -exec rm -r "{}" \;
+sudo find / -name ".node_*" -exec rm -r "{}" \;
+sudo find / -name ".node.*" -exec rm -r "{}" \;
+
+sudo find / -name "npm" -exec rm -r "{}" \;
+sudo find / -name "npm-*" -exec rm -r "{}" \;
+sudo find / -name "npm*" -exec rm -r "{}" \;
+sudo find / -name "npm.*" -exec rm -r "{}" \;
+
+sudo find / -name ".npm" -exec rm -r "{}" \;
+sudo find / -name ".npm-*" -exec rm -r "{}" \;
+sudo find / -name ".npm*" -exec rm -r "{}" \;
+sudo find / -name ".npm.*" -exec rm -r "{}" \;
+
+#sudo find / -name "nvm" -o -name ".nvm" -o -name "node" -o -name ".node" -o -name "node_modules" -o -name ".npm"  -o -name "npm*" -exec rm -r "{}" \;
 
 echo "Finding node after deleting";
 node -v;
