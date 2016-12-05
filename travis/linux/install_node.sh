b01f42b945200ba1c847fec8fad3c069ccd934b5
illichso <illichso@gmail.com>
2016-12-05 16:42:20 +0100

    add apt-get update

--- travis/linux/install_node.sh
+++ travis/linux/install_node.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+sudo apt-get -qq update;
 sudo apt-get -qq purge --auto-remove nodejs;
 sudo curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -;
 sudo apt-get -qq install -y nodejs;
