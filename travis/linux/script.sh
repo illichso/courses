29606d0a8983ebe5ed5304fb4f076d26c1c24b31
illichso <illichso@gmail.com>
2016-12-05 16:21:37 +0100

    remove ls from travis configs

--- travis/linux/script.sh
+++ travis/linux/script.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 
 pwd;
-ls -la -a;
 bash ./travis/delete_node.sh;
 
 pwd;
-ls -la -a;
 chmod +x ./travis/linux/install_node.sh;
 bash ./travis/linux/install_node.sh;
--- travis/macos/script.sh
+++ travis/macos/script.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env sh
 
 pwd;
-ls -la -a;
 chmod +x ./travis/macos/predelete_node.sh;
 bash ./travis/macos/predelete_node.sh;
 
 pwd;
-ls -la -a;
 bash ./travis/delete_node.sh;
 
 pwd;
-ls -la -a;
 chmod +x ./travis/macos/install_node.sh;
 bash ./travis/macos/install_node.sh;
