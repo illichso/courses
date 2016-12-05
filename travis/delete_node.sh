6bb0008e6bc20e5ecccc4d84416a032f6396a3e6
illichso <illichso@gmail.com>
2016-12-05 12:42:38 +0100

    use ndoe delete script successful faor mac

--- travis/delete_node.sh
+++ travis/delete_node.sh
@@ -3,36 +3,18 @@
 echo "Finding node before deleting";
 node -v;
 npm -v;
-echo "Finding and deleting node, node_modules, npm";
+#echo "Finding and deleting node, node_modules, npm";
 
-#rm -rf /usr/local/lib/node;
-#rm -rf /usr/local/lib/node_modules;
-#rm -rf /usr/local/include/node;
-#rm -rf /usr/local/include/node_modules;
-#rm -rf ~/local/node;
-#rm -rf ~/local/node_modules;
-#rm -rf ~/lib/node;
-#rm -rf ~/lib/node_modules;
-#rm -rf ~/include/node;
-#rm -rf ~/include/node_modules;
-
-
-
-
-sudo rm -rf /usr/local/lib/node;
-sudo rm -rf /usr/local/lib/node_modules;
-sudo rm -rf /usr/local/include/node;
-sudo rm -rf /usr/local/include/node_modules;
-sudo rm -rf ~/local/node;
-sudo rm -rf ~/local/node_modules;
-sudo rm -rf ~/lib/node;
-sudo rm -rf ~/lib/node_modules;
-sudo rm -rf ~/include/node;
-sudo rm -rf ~/include/node_modules;
-sudo rm -rf /usr/local/bin/node;
-sudo rm -rf /usr/local/bin/npm;
-sudo rm -rf /usr/local/lib/dtrace/node*;
-sudo rm -rf ~/.npm;
+echo "Removing previously installed nodejs"
+sudo rm -rf /usr/local/lib/node*;
+sudo rm -rf /usr/local/include/node*;
+echo "Manually removing node folders"
+sudo rm -rf ~/local/node*;
+sudo rm -rf ~/lib/node*;
+sudo rm -rf ~/include/node*;
+sudo rm -rf /usr/local/bin/node*;
+sudo rm -rf /usr/local/bin/npm*;
+sudo rm -rf /usr/local/lib/dtrace/node.d;
 sudo rm -rf /usr/local/share/man/man1/node*;
 sudo rm -rf /usr/local/share/man/man1/npm*;
 sudo rm -rf /usr/local/share/man/man2/node*;
@@ -51,43 +33,17 @@ sudo rm -rf /usr/local/share/man/man8/node*;
 sudo rm -rf /usr/local/share/man/man8/npm*;
 sudo rm -rf /usr/local/share/man/man9/node*;
 sudo rm -rf /usr/local/share/man/man9/npm*;
+sudo rm -rf /usr/local/lib/dtrace/node.d;
 sudo rm -rf ~/.npm;
-sudo rm -rf ~/.node;
-sudo rm -rf ~/.node-gyp;
+sudo rm -rf ~/.node*;
 sudo rm -rf /opt/local/bin/node;
 sudo rm -rf /opt/local/include/node;
-sudo rm -rf /opt/local/lib/node_modules;
+sudo rm -rf /opt/local/lib/node*;
 sudo rm -rf /usr/bin/npm;
 sudo rm -rf /usr/local/share/systemtap/tapset/node.stp;
 sudo rm -rf /var/db/receipts/org.nodejs.*;
-
-echo "Find node after deleting";
-
-echo "sudo find / -name '.npm'"
-sudo find / -name ".npm"
-
-echo "sudo find / -name 'npm*'"
-sudo find / -name "npm*"
-
-echo "sudo find / -name 'node_modules'"
-sudo find / -name "node_modules"
-
-echo "sudo find / -name 'node.*'"
-sudo find / -name "node.*"
-
-echo "sudo find / -name 'node'"
-sudo find / -name "node"
-
-
-
-#echo "Launching sudo find / -name 'node' -exec rm -r '{}' \;"
-#sudo find / -name "node" -exec rm -r "{}" \;
-#
-#echo "Launching sudo find / -name 'node_modules' -exec rm -r '{}' \;"
-#sudo find / -name "node_modules" -exec rm -r "{}" \;
-#
-#echo "Launching sudo find / -name 'npm*' -exec rm -r '{}' \;"
-#sudo find / -name "npm*" -exec rm -r "{}" \;
+sudo rm -rf ~/.nvm;
+sudo rm -rf /usr/local/Cellar/node;
 
 echo "Finding node after deleting";
 echo "Trying node -v";
