846a958ffa009573bfad07471b911fd5bbdfe332
illichso <illichso@gmail.com>
2016-12-05 09:53:24 +0100

    reduce delete node script

--- travis/delete_node.sh
+++ travis/delete_node.sh
@@ -4,37 +4,10 @@ echo "Finding node before deleting";
 node -v;
 npm -v;
 echo "Finding and deleting node, node_modules, npm";
-#sudo find / -name "nvm" -exec rm -r "{}" \;
-#sudo find / -name "nvm-*" -exec rm -r "{}" \;
-#sudo find / -name "nvm_*" -exec rm -r "{}" \;
-#sudo find / -name "nvm.*" -exec rm -r "{}" \;
-
-sudo find / -name ".node" -exec rm -r "{}" \;
-sudo find / -name ".node-*" -exec rm -r "{}" \;
-sudo find / -name ".node_*" -exec rm -r "{}" \;
-sudo find / -name ".node.*" -exec rm -r "{}" \;
 
 sudo find / -name "node" -exec rm -r "{}" \;
-sudo find / -name "node-*" -exec rm -r "{}" \;
-sudo find / -name "node_*" -exec rm -r "{}" \;
-sudo find / -name "node.*" -exec rm -r "{}" \;
-
-sudo find / -name ".node" -exec rm -r "{}" \;
-sudo find / -name ".node-*" -exec rm -r "{}" \;
-sudo find / -name ".node_*" -exec rm -r "{}" \;
-sudo find / -name ".node.*" -exec rm -r "{}" \;
-
-sudo find / -name "npm" -exec rm -r "{}" \;
-sudo find / -name "npm-*" -exec rm -r "{}" \;
-sudo find / -name "npm_*" -exec rm -r "{}" \;
-sudo find / -name "npm.*" -exec rm -r "{}" \;
-
-sudo find / -name ".npm" -exec rm -r "{}" \;
-sudo find / -name ".npm-*" -exec rm -r "{}" \;
-sudo find / -name ".npm_*" -exec rm -r "{}" \;
-sudo find / -name ".npm.*" -exec rm -r "{}" \;
-
-#sudo find / -name "nvm" -o -name ".nvm" -o -name "node" -o -name ".node" -o -name "node_modules" -o -name ".npm"  -o -name "npm*" -exec rm -r "{}" \;
+sudo find / -name "node_modules" -exec rm -r "{}" \;
+sudo find / -name "npm*" -exec rm -r "{}" \;
 
 echo "Finding node after deleting";
 echo "Trying node -v";
